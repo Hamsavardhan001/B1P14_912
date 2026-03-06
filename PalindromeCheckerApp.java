23a2a9e5d82ed3b60ce165a85abe1cec747af5ec
Hamsavardhan T S <hamsavardhan.ts@gmail.com>
2026-03-06 16:17:06 +0530

    Third

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -3,7 +3,12 @@ import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        System.out.println("Starting up....");
-        System.out.println("Palindrome App started :)");
+        String str = "malayalam";
+        int i = 0; int j = str.length() - 1;
+        while(i<j){
+            if(str.charAt(i) != str.charAt(j)) return;
+            i++; j--;
+        }
+        System.out.println("Palindrome");
     }
 }
