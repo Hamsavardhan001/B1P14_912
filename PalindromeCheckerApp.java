c2f94ba9881d17bdcb031019dd903336822c0e87
Hamsavardhan T S <hamsavardhan.ts@gmail.com>
2026-03-06 16:30:22 +0530

    Fifth

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,17 +1,16 @@
 package PalindromeApps;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         String str = "malayalam";
-        char[] chars = str.toCharArray();
-        int i = 0;
-        int j = chars.length - 1;
-
-        while(i<j){
-            if(chars[i] != chars[j]) return;
-            i++;
-            j--;
+        Stack<Character> stk = new Stack<>();
+        for(char c : str.toCharArray()){
+            stk.push(c);
+        }
+        for(int i = 0; i < str.length() -1; i++){
+            if(str.charAt(i) != stk.pop()) return;
         }
 
         System.out.println("Palindrome");
